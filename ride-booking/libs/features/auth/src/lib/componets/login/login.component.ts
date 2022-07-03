39e766618c75c21fc90ca9afb6a9ebdce747f43b
noellamwanzia <noellamwanzia@gmail.com>
2022-07-03 18:55:05 +0300

    ✨Create login page.

--- ride-booking/libs/features/auth/src/lib/componets/login/login.component.ts
+++ ride-booking/libs/features/auth/src/lib/componets/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { Router } from '@angular/router';
 
 @Component({
   selector: 'ride-booking-login',
@@ -6,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  
+  logInForm!: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(  private _fb: FormBuilder ) { }
+
+  ngOnInit(): void {
+    this.initForm()
+  }
+
+  initForm()
+  {
+    this.logInForm = this._fb.group({
+      "email": ['',[Validators.required]],
+      "password": ['', Validators.required]
+    })
+  }
+
+  login()
+  {
+    // const email = this.logInForm.controls['email'].value;
+    // const password = this.logInForm.controls['password'].value;
+
+    // this._authService.loginWithEmailAndPassword(email,password)
+    //                   .then((res) => this._router.navigate['/home'])
+    //                   .catch((err) => console.log(err))
+  }
 }
