20e3ba6584d610e928db201b6da19e123c7653ab
noellamwanzia <noellamwanzia@gmail.com>
2022-07-03 16:35:20 +0300

    🔧separate the routes array.

--- ride-booking/apps/passenger-app/src/app/app.module.ts
+++ ride-booking/apps/passenger-app/src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AuthModule, authRoutes } from '@ride-booking/features/auth';
+
+const routes: Routes = [
+  {path: '', children: authRoutes}
+];
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([{path: 'auth', children: authRoutes}], { initialNavigation: 'enabledBlocking' }),
+    RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
     AuthModule
   ],
   providers: [],
